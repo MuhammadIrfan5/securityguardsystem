11ae3fcea44739be010c3a48d2a02b9512bbddc3
Sohail Ali <sohailali01@outllok.com>
2023-10-04 02:54:00 +0500

    migration

--- database/migrations/2023_10_03_000000_create_users_table.php
+++ database/migrations/2023_10_03_000000_create_users_table.php
@@ -13,10 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
             $table->string('email')->unique();
+            $table->string('secondary_email')->nullable();
+            $table->string('phone1')->unique();
+            $table->string('phone2')->nullable();
+            $table->string('address');
+            $table->string('dob');
+            $table->string('age')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('opt')->nullable();
+            $table->string('opt_expiry')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
