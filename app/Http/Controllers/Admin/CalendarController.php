1db19bb68937ab263156e170cce9def4f7c471cf
Sohail Ali <sohail.ali@zilliondesigns.com>
2023-12-04 11:03:35 +0500

    g

--- app/Http/Controllers/Admin/CalendarController.php
+++ app/Http/Controllers/Admin/CalendarController.php
@@ -25,10 +25,10 @@ class CalendarController extends Controller
                 ->get();
             foreach ($data as $key => $item) {
                 $list[] =
-                    ['id'    => $item->id,
-                     'title' => 'SOHAIL' . $key,
-                     'start' => $item->start_date,
-                     'end'   => $item->end_date
+                    [ 'id'    => $item->id,
+                      'title' => 'SOHAIL' . date('hA', strtotime($item->start_time)) . '-to-' . date('hA', strtotime($item->end_time)),
+                      'start' => $item->start_date,
+                      'end'   => $item->end_date
                     ];
             }
 
@@ -42,12 +42,14 @@ class CalendarController extends Controller
         switch ($request->type) {
             case 'add':
                 $event = Schedule::create([
-                        'location_id' => $request->location,
-                        'employee_id' => $request->employee,
-                        'start_date'  => $request->start,
-                        'end_date'    => $request->end,
-                        'comments'    => $request->comments ?? "",
-                        'created_by'  => $request->user()['id'],
+                    'location_id' => $request->location,
+                    'employee_id' => $request->employee,
+                    'start_date'  => $request->start,
+                    'end_date'    => $request->end,
+                    'start_time'  => $request->startTime,
+                    'end_time'    => $request->endTime,
+                    'comments'    => $request->comments ?? "",
+                    'created_by'  => $request->user()['id'],
                 ]);
 
                 return response()->json($event);
@@ -55,11 +57,13 @@ class CalendarController extends Controller
 
             case 'update':
                 $events = Schedule::find($request->id);
-                $event = Schedule::create([
+                $event  = Schedule::create([
                     'location_id' => $events->location_id,
                     'employee_id' => $events->employee_id,
                     'start_date'  => $events->start_date,
                     'end_date'    => $events->end_date,
+                    'start_time'  => $events->startTime,
+                    'end_time'    => $events->endTime,
                     'comments'    => $events->comments ?? "",
                     'created_by'  => $request->user()['id'],
                 ]);
@@ -83,7 +87,7 @@ class CalendarController extends Controller
     {
         $data['location'] = Location::all();
         $data['employee'] = Employee::all();
-        $data['days'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        $data['days']     = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
         return response($data);
     }
 }
