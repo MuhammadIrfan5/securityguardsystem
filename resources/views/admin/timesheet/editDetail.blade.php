4b1912c1053fc5538c83503566a9f2727140f2b3
Sohail Ali <sohail.ali@zilliondesigns.com>
2024-05-13 13:08:26 +0500

    g

--- resources/views/admin/timesheet/editDetail.blade.php
+++ resources/views/admin/timesheet/editDetail.blade.php
@@ -123,6 +123,7 @@
                     <h5 class="modal-title">Add Time Sheet</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                <div class="modal-body">
                 <form id="addUpdates" method="post">
                     <div class="modal-body">
                         <div class="row">
