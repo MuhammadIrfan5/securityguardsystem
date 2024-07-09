bf6508ceda639fb8366a85a61a25955e4b97a598
Sohail Ali <sohail.ali@zilliondesigns.com>
2024-07-09 16:47:11 +0500

    g

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -25,36 +25,6 @@
                         <h6>{{request()->user()->firstname}}</h6>
                         <span>{{request()->user()->firstname}}</span>
                     </li>
-                    {{--                    <li>--}}
-                    {{--                        <hr class="dropdown-divider">--}}
-                    {{--                    </li>--}}
-
-                    {{--                    <li>--}}
-                    {{--                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">--}}
-                    {{--                            <i class="bi bi-person"></i>--}}
-                    {{--                            <span>My Profile</span>--}}
-                    {{--                        </a>--}}
-                    {{--                    </li>--}}
-                    {{--                    <li>--}}
-                    {{--                        <hr class="dropdown-divider">--}}
-                    {{--                    </li>--}}
-
-                    {{--                    <li>--}}
-                    {{--                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">--}}
-                    {{--                            <i class="bi bi-gear"></i>--}}
-                    {{--                            <span>Account Settings</span>--}}
-                    {{--                        </a>--}}
-                    {{--                    </li>--}}
-                    {{--                    <li>--}}
-                    {{--                        <hr class="dropdown-divider">--}}
-                    {{--                    </li>--}}
-
-                    {{--                    <li>--}}
-                    {{--                        <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">--}}
-                    {{--                            <i class="bi bi-question-circle"></i>--}}
-                    {{--                            <span>Need Help?</span>--}}
-                    {{--                        </a>--}}
-                    {{--                    </li>--}}
                     <li>
                         <hr class="dropdown-divider">
                     </li>
