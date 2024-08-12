dbda7985750c5b371baf9078210dbdd33fc1f6a2
Sohail Ali <sohail.ali@zilliondesigns.com>
2024-08-12 18:55:55 +0500

    g

--- app/Exports/ExportConfirmationCall.php
+++ app/Exports/ExportConfirmationCall.php
@@ -4,14 +4,52 @@ namespace App\Exports;
 
 use App\Models\ConfirmationCall;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExportConfirmationCall implements FromCollection
+class ExportConfirmationCall implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return ConfirmationCall::all();
+        return ConfirmationCall::with(['user','employee']) // Eager load the user relationship
+        ->get()
+            ->map(function ($confirmationCall) {
+                return [
+                    'ID'            => $confirmationCall->id,
+                    'User Name'     => $confirmationCall->user->first_name,
+                    'Time Sheet ID' => $confirmationCall->time_sheet_id,
+                    'Schedule ID'   => $confirmationCall->schedule_id,
+                    'Location ID'   => $confirmationCall->location->name,
+                    'Employee ID'   => $confirmationCall->employee->name,
+                    'Gate Combo'    => $confirmationCall->gate_combo,
+                    'Call Time'     => $confirmationCall->call_time,
+                    'Post Phone'    => $confirmationCall->post_phone,
+                    'Status'        => $confirmationCall->status,
+                    'Notes'         => $confirmationCall->notes,
+                    'Created At'    => $confirmationCall->created_at,
+                    'Updated At'    => $confirmationCall->updated_at,
+                ];
+            });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'User Name', // Updated heading
+            'Time Sheet ID',
+            'Schedule ID',
+            'Location ID',
+            'Employee ID',
+            'Gate Combo',
+            'Call Time',
+            'Post Phone',
+            'Status',
+            'Notes',
+            'Created At',
+            'Updated At',
+        ];
     }
 }
